b920b76feae906dc3d3a564b958bc43ded9ec48a
Brian Ma <514972100@qq.com>
2016-04-08 13:52:04 +0800

    枚举在编译时检查类型

--- src/main/java/base/reflection/enumeration/EnumTest.java
+++ src/main/java/base/reflection/enumeration/EnumTest.java
@@ -6,23 +6,6 @@ package base.reflection.enumeration;
 public class EnumTest {
     public static void main(String[] args) {
         Week week = Week.SAT;
-        switch (week){
-            case MON : System.out.println("忙day");
-                break;
-            case TUE : System.out.println("求死day");
-                break;
-            case WED : System.out.println("未死day");
-                break;
-            case THR : System.out.println("受死day");
-                break;
-            case FRI : System.out.println("福来day");
-                break;
-            case SAT : System.out.println("福来day");
-                break;
-            case SUN : System.out.println("福来day");
-                break;
-            default:
-                System.out.println();
-        }
+        new WeekTest().choose(week);
     }
 }
