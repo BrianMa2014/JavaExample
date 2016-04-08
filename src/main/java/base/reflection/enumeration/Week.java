08d5d363fc9663a2039af6a8efd10a18417b39dd
Brian Ma <514972100@qq.com>
2016-04-08 10:36:56 +0800

    分号

--- src/main/java/base/reflection/enumeration/Week.java
+++ src/main/java/base/reflection/enumeration/Week.java
@@ -4,6 +4,6 @@ package base.reflection.enumeration;
  * Created by MaMingJiang on 2016/4/8.
  */
 public enum Week {
-    MON,TUE,WED,THR,FRI,SAT,SUN;
+    MON,TUE,WED,THR,FRI,SAT,SUN  //可以不加分号；
 }
 
